a83f0329977f957614e5b7580cf451120b0b9586
Zhida Zhang <zhangzhida822@outlook.com>
2018-12-14 00:03:44 -0500

    find tree height

---
+++ BinaryTree_FindTreeHeight/FindTreeHeight.java
@@ -0,0 +1,23 @@
+// Time: O(n)
+// Space: O(Height of tree)
+
+
+/**
+ * public class TreeNode {
+ *   public int key;
+ *   public TreeNode left;
+ *   public TreeNode right;
+ *   public TreeNode(int key) {
+ *     this.key = key;
+ *   }
+ * }
+ */
+public class Solution {
+    public int findHeight(TreeNode root) {
+      // Write your solution here
+      if (root == null) {
+        return 0;
+      }
+      return Math.max(findHeight(root.left), findHeight(root.right)) + 1;
+    }
+  }
