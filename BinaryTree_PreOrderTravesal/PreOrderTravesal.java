93b0650579571cf00a76e13c5ade7a7ed5dbc8e0
Zhida Zhang <zhangzhida822@outlook.com>
2018-12-14 01:04:21 -0500

    add iterative implementation

--- BinaryTree_PreOrderTravesal/PreOrderTravesal.java
+++ BinaryTree_PreOrderTravesal/PreOrderTravesal.java
@@ -12,7 +12,8 @@
  * }
  */
 public class Solution {
-    public List<Integer> preOrder(TreeNode root) {
+    // recursion implementation
+    public List<Integer> preOrder1(TreeNode root) {
       // Write your solution here
       List<Integer> list = new LinkedList<>();
       helper(root, list);
@@ -28,4 +29,26 @@ public class Solution {
       helper(root.right, list);
       return;
     }
+
+    // iterative implementation
+    public List<Integer> preOrder2(TreeNode root) {
+        // Write your solution here
+        List<Integer> list = new ArrayList<>();
+        if (root == null) {
+          return list;
+        }
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.offerFirst(root);
+        while (!stack.isEmpty()) {
+          TreeNode node = stack.pollFirst();
+          if (node.right != null) {
+            stack.offerFirst(node.right);
+          }
+          if (node.left != null) {
+            stack.offerFirst(node.left);
+          }
+          list.add(node.key);
+        }
+        return list;
+      }
   }
