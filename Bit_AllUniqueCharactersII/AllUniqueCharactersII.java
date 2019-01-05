cbcef5e638c7fbc5a0cf8f1bcb82ccdafedce558
Zhida Zhang <zhangzhida822@outlook.com>
2019-01-05 21:35:28 +0800

    unique characters, method 2

--- Bit_AllUniqueCharactersII/AllUniqueCharactersII.java
+++ Bit_AllUniqueCharactersII/AllUniqueCharactersII.java
@@ -16,4 +16,20 @@ public class Solution {
       }
       return true;
     }
+
+    public boolean allUnique2(String word) {
+        char[] array = word.toCharArray();
+        int[] bit_vector = new int[8];
+        for (char c : array) {
+            int row = c / 32;
+            int col = c - c / 32 * 32;
+            int test = 1 << col;
+            if ((bit_vector[row] & test) != 0) {
+                return false;
+            } else {
+                bit_vector[row] = bit_vector[row] | test;
+            }
+        }
+        return true;
+    }
   }
